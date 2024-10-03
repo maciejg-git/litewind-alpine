737a6316f5c5a5679e3a147d29e2fb72a74aa6ef
maciejg-git <ender@localhost>
2024-10-04 01:04:39 +0200

    update

--- datepicker/datepicker.js
+++ datepicker/datepicker.js
@@ -38,12 +38,13 @@ document.addEventListener("alpine:init", () => {
       locale: "en-GB",
       mondayFirstWeekday: true,
       adjacentMonthsDays: true,
-      range: true,
+      range: false,
       _model: null,
       selectedSingle: null,
       selectedRange: [],
       rangeState: 0,
       mouseOverDate: null,
+      modelSeparator: "-",
 
       init() {
         this.month = this.today.getMonth();
@@ -70,10 +71,11 @@ document.addEventListener("alpine:init", () => {
           this.range = isFunction(props.range)
             ? props.range()
             : props.range ?? this.range;
+          this.reset()
         });
 
         Alpine.effect(() => {
-          let dateRegexp = /^\d\d\d\d-\d\d-\d\d$/;
+          let dateRegexp = /^\d{4}-\d{2}-\d{2}$/;
 
           if (this.range && this._model?.length === 2) {
             if (this._model.every((d) => dateRegexp.test(d))) {
