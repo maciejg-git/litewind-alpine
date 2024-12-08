d4dc6294ccb2013add5c86437c0ccd21252e6f90
maciejg-git <ender@localhost>
2024-12-08 01:50:45 +0100

    add interactive prop

--- progress/progress.js
+++ progress/progress.js
@@ -1,11 +1,18 @@
 document.addEventListener("alpine:init", () => {
-  Alpine.data("progress", (defaults = {}) => {
+  Alpine.data("progress", (props = {}) => {
     return {
       value: 0,
+      interactive: props?.interactive ?? false,
 
       init() {
         Alpine.bind(this.$el, {
           "x-modelable": "value",
+          "@click"() {
+            if (!this.interactive) return
+            let ev = this.$event
+            let x = (ev.x - ev.target.offsetLeft) / ev.target.clientWidth
+            this.value = x * 100
+          }
         });
       },
       progressBar: {
