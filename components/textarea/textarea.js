36d1ef0f4a40e727161100916feb3acc188b3fac
maciejg-git <ender@localhost>
2025-02-20 01:11:20 +0100

    update

--- components/textarea/textarea.js
+++ components/textarea/textarea.js
@@ -8,47 +8,48 @@ export default function (Alpine) {
       init() {
         this.$nextTick(() => {
           Alpine.effect(() => {
-            this.placeholder = Alpine.bound(this.$el, "data-placeholder") ?? this.placeholder
-          })
+            this.placeholder =
+              Alpine.bound(this.$el, "data-placeholder") ?? this.placeholder;
+          });
 
           Alpine.bind(this.$el, {
             ":class"() {
-              let classes = this.$el.attributes
-              let c = ""
+              let classes = this.$el.attributes;
+              let c = "";
               if (this.validation?.state === "valid") {
-                c = classes["class-valid"]?.textContent || ""
+                c = classes["class-valid"]?.textContent || "";
               } else if (this.validation?.state === "invalid") {
-                c = classes["class-invalid"]?.textContent || ""
+                c = classes["class-invalid"]?.textContent || "";
               } else {
-                c = classes["class-default"]?.textContent || ""
+                c = classes["class-default"]?.textContent || "";
               }
 
-              return c
-            }
-          })
-        })
+              return c;
+            },
+          });
+        });
         Alpine.bind(this.$el, {
           ["x-modelable"]: "_value",
           ["@mousedown.prevent"]() {
-            this.$refs.textarea.focus()
-          }
-        })
+            this.$refs.textarea.focus();
+          },
+        });
       },
       textarea: {
         ":value"() {
-          return this._value
+          return this._value;
         },
         "@input"() {
-          this._value = this.$event.target.value
+          this._value = this.$event.target.value;
         },
         "x-ref": "textarea",
         ":placeholder"() {
-          return this.placeholder
+          return this.placeholder;
         },
         "@blur"() {
-          if (typeof this.touch === "function") this.touch()
-        }
-      }
-    }
-  })
+          if (typeof this.touch === "function") this.touch();
+        },
+      },
+    };
+  });
 }
