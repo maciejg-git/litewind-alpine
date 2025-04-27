b3fa7fc01b5e84905bd7ef84d0cec92ca1694e77
maciejg-git <m.gralek@gmail.com>
2025-04-27 02:29:55 +0200

    update autocomplete

--- components/autocomplete/autocomplete.js
+++ components/autocomplete/autocomplete.js
@@ -97,9 +97,10 @@ export default function (Alpine) {
 
         Alpine.bind(this.$el, {
           ["x-modelable"]: "_model",
-          ["@keydown.prevent.down"]() {
+          async ["@keydown.prevent.down"]() {
             if (!this.isOpen) {
               this.open();
+              await this.$nextTick()
             }
             if (this.highlightedIndex >= this.getItems().length - 1) {
               return;
