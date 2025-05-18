7b6f97ea0b676949a3c90cefdd6c99eba28581ef
maciejg-git <m.gralek@gmail.com>
2025-05-18 14:18:49 +0200

    update range

--- components/range/range.js
+++ components/range/range.js
@@ -1,8 +1,7 @@
 export default function (Alpine) {
   Alpine.data("range", () => {
     return {
-      _valueMin: 0,
-      _valueMax: 0,
+      _value: 0,
       _onMousemove: null,
       _lastX: 0,
       _valueX: 0,
@@ -21,8 +20,8 @@ export default function (Alpine) {
         })
       },
       handleMousedown() {
-        this.$refs.sliderMax.focus()
-        this._valueMax = this.$event.offsetX / this.$el.clientWidth
+        this.$refs.slider.focus()
+        this._value = this.$event.offsetX / this.$el.clientWidth
         window.addEventListener("mousemove", this._onMousemove)
         this._lastX = this.$event.pageX
         this._valueX = this.$event.offsetX
@@ -37,33 +36,24 @@ export default function (Alpine) {
         let movementX = event.pageX - this._lastX
         this._currentX = this._currentX + movementX
         this._lastX = event.pageX
-        this._valueMax = (this._currentX + this._valueX) / this.$el.clientWidth
-        this._valueMax = this._valueMax <= 0 ? 0 : this._valueMax >= 1 ? 1 : this._valueMax
+        this._value = (this._currentX + this._valueX) / this.$el.clientWidth
+        this._value = this._value <= 0 ? 0 : this._value >= 1 ? 1 : this._value
       },
       trackFill: {
         ":style"() {
           return {
-            width: (this._valueMax * 100) + "%"
+            width: (this._value * 100) + "%"
           }
         }
       },
-      sliderMin: {
+      slider: {
         ":style"() {
           return {
-            left: (this._valueMin * 100) + "%",
+            left: (this._value * 100) + "%",
             transform: "translateX(-50%)",
           }
         },
-        "x-ref": "sliderMax",
-      },
-      sliderMax: {
-        ":style"() {
-          return {
-            left: (this._valueMax * 100) + "%",
-            transform: "translateX(-50%)",
-          }
-        },
-        "x-ref": "sliderMax",
+        "x-ref": "slider",
       }
     }
   })
