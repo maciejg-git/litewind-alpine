aa3c9d8cbb0c020ed9020e9319b931fc5529cd24
maciejg-git <m.gralek@gmail.com>
2025-05-20 03:58:42 +0200

    update range

--- components/range/range.js
+++ components/range/range.js
@@ -1,11 +1,17 @@
 export default function (Alpine) {
+  let clamp = (value, min, max) => value <= min ? min : value >= max ? max : value
+
+  let getStep = (value, steps) => Math.round(value * steps) * (1 / steps)
+
   Alpine.data("range", () => {
     return {
       _slider1: {
         value: 0,
+        name: "slider1",
       },
       _slider2: {
         value: 0,
+        name: "slider2",
       },
       _currentSlider: null,
       _onMousemove: null,
@@ -14,27 +20,32 @@ export default function (Alpine) {
       _currentX: 0,
       _min: 0,
       _max: 100,
+      _range: 0,
       _step: 1,
       _steps: 0,
       _fixedMin: false,
+      _maxValue: 0,
 
       init() {
         this._onMousemove = this.handleMousmove.bind(this)
 
         this.$nextTick(() => {
-          this._min = parseInt(
+          this._min = parseFloat(
             Alpine.bound(this.$el, "data-min") ?? this._min
           )
-          this._max = parseInt(
+          this._max = parseFloat(
             Alpine.bound(this.$el, "data-max") ?? this._max
           )
-          this._step = parseInt(
+          this._step = parseFloat(
             Alpine.bound(this.$el, "data-step") ?? this._step
           )
-          this._steps = (this._max - this._min) / this._step
           this._fixedMin = JSON.parse(
             Alpine.bound(this.$el, "data-fixed-min") ?? this._fixedMin
           )
+
+          this._range = this._max - this._min
+          this._steps = this._range / this._step
+          this._maxValue = (Math.floor(this._steps) * this._step) / this._range
         })
 
         Alpine.bind(this.$el, {
@@ -48,16 +59,9 @@ export default function (Alpine) {
       },
       handleMousedown() {
         let value = this.$event.offsetX / this.$el.clientWidth
-        let dist1 = Math.abs(value - this._slider1.value)
-        let dist2 = Math.abs(value - this._slider2.value)
-        if (!this._fixedMin && dist1 < dist2) {
-          this._currentSlider = this._slider1
-          this.$refs.slider1.focus()
-        } else {
-          this._currentSlider = this._slider2
-          this.$refs.slider2.focus()
-        }
-        this._currentSlider.value = value
+        this._currentSlider = this.getClosestSlider(value)
+        this.$refs[this._currentSlider.name].focus()
+        this._currentSlider.value = getStep(value, this._steps)
         window.addEventListener("mousemove", this._onMousemove)
         this._lastX = this.$event.pageX
         this._valueX = this.$event.offsetX
@@ -73,8 +77,29 @@ export default function (Alpine) {
         this._currentX = this._currentX + movementX
         this._lastX = event.pageX
         let value = (this._currentX + this._valueX) / this.$el.clientWidth
-        value = Math.round(value * this._steps) * (1 / this._steps)
-        this._currentSlider.value = value <= 0 ? 0 : value >= 1 ? 1 : value
+        value = getStep(value, this._steps)
+        this._currentSlider.value = clamp(value, 0, this._maxValue)
+      },
+      getClosestSlider(value) {
+        if (this._fixedMin) {
+          return this._slider2
+        }
+        let dist1 = Math.abs(value - this._slider1.value)
+        let dist2 = Math.abs(value - this._slider2.value)
+        if (dist1 < dist2) {
+          return this._slider1
+        } else {
+          return this._slider2
+        }
+      },
+      getValue1() {
+        return (this._slider1.value * this._range) + this._min
+      },
+      getValue2() {
+        return (this._slider2.value * this._range) + this._min
+      },
+      getSteps() {
+        return Math.floor(this._steps)
       },
       trackFill: {
         ":style"() {
