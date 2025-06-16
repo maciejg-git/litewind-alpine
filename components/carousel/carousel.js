6fb6857cbc2acedfae22913b94176f062351ffe6
maciejg-git <m.gralek@gmail.com>
2025-06-16 02:49:11 +0200

    update carousel

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -1,58 +1,89 @@
 export default function (Alpine) {
   Alpine.data("carousel", () => {
     return {
-      _items: [],
       _currentIndex: 0,
       _direction: false,
+      _timer: null,
+      // props
+      _items: [],
+      _autoPlay: false,
+      _autoPlayDelay: 5000,
+
       init() {
         this.$nextTick(() => {
-          this._items = Alpine.bound(this.$el, "data-items") ?? this._items
-        })
+          Alpine.effect(() => {
+            this._items = Alpine.bound(this.$el, "data-items") ?? this._items;
+            this._currentIndex = 0;
+            if (this._autoPlay) {
+              this.stopAutoPlay();
+              this.startAutoPlay();
+            }
+          });
+          let autoPlayDelay = Alpine.bound(this.$el, "data-auto-play") ?? false;
+          this._autoPlayDelay =
+            autoPlayDelay === true
+              ? this._autoPlayDelay
+              : autoPlayDelay === false
+                ? 0
+                : parseInt(autoPlayDelay);
+          this._autoPlay = !!this._autoPlayDelay;
+          if (this._autoPlay) {
+            this.startAutoPlay();
+          }
+        });
       },
       getItems() {
-        return this._items
+        return this._items;
       },
       showNext() {
-        this._currentIndex++
+        this._currentIndex++;
         if (this._currentIndex >= this._items.length) {
-          this._currentIndex = 0
+          this._currentIndex = 0;
         }
-        this._direction = true
+        this._direction = true;
       },
       showPrev() {
-        this._currentIndex--
+        this._currentIndex--;
         if (this._currentIndex < 0) {
-          this._currentIndex = this._items.length - 1
+          this._currentIndex = this._items.length - 1;
         }
-        this._direction = false
+        this._direction = false;
       },
       setCurrent() {
-        this._currentIndex = this.index
+        this._currentIndex = this.index;
+      },
+      startAutoPlay() {
+        this._timer = setInterval(() => {
+          this.showNext();
+        }, this._autoPlayDelay);
+      },
+      stopAutoPlay() {
+        clearInterval(this._timer);
       },
       item: {
         "x-show"() {
-          return this.index === this._currentIndex
+          return this.index === this._currentIndex;
         },
         ":data-direction"() {
-          return this._direction
-        }
+          return this._direction;
+        },
       },
       indicator: {
         "@click"() {
-          this.setCurrent()
+          this.setCurrent();
         },
         ":class"() {
-          let classes = this.$el.attributes
-          let c = ""
+          let classes = this.$el.attributes;
+          let c = "";
           if (this.index === this._currentIndex) {
-            c = classes["class-current"]?.textContent || ""
+            c = classes["class-current"]?.textContent || "";
           } else {
-            c = classes["class-default"]?.textContent || ""
+            c = classes["class-default"]?.textContent || "";
           }
 
-          return c
-        }
-      }
-    }
-  })
+          return c;
+        },
+      },
+    };
+  });
 }
